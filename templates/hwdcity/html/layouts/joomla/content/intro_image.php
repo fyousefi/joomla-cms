e01557934fb0f97983b26d487c124d8197f37c8a
fyousefi <f.yousefi2@gmail.com>
2025-09-19 14:09:14 +0330

    refactor(template): add h-65 class for 260px height in 400px img

--- templates/hwdcity/html/layouts/joomla/content/intro_image.php
+++ templates/hwdcity/html/layouts/joomla/content/intro_image.php
@@ -27,7 +27,7 @@ $layoutAttr = [
     'alt' => empty($images->image_intro_alt) && empty($images->image_intro_alt_empty) ? false : $images->image_intro_alt,
 ];
 ?>
-<figure class="<?php echo $this->escape($imgclass); ?> item-image position-relative mb-3">
+<figure class="<?php echo $this->escape($imgclass); ?> item-image position-relative mb-3 h-65">
     <?php if ($params->get('link_intro_image') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
         <a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>" title="<?php echo $this->escape($displayData->title); ?>">
             <span class="triangle-up position-absolute bottom-0 mx-5"></span>
