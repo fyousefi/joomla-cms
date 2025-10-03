5fd8a67a54ad6f60bb7cada04bed887a8d037f89
fyousefi <f.yousefi2@gmail.com>
2025-10-03 19:58:48 +0330

    feat(template): modernize design for remind username layout

--- templates/hwdcity/html/com_users/remind/default.php
+++ templates/hwdcity/html/com_users/remind/default.php
@@ -26,7 +26,7 @@ $back = (is_string($ref) && str_starts_with($ref, $root)) ? $ref : Route::_('ind
 ?>
 <div class="container mt-8">
     <div class="com-users-remind remind row justify-content-center">
-        <div class="col-sm-12 col-md-9 col-lg-4 bg-body border border-1 rounded p-0 fs-8">
+        <div class="col-sm-12 col-md-9 col-lg-4 p-0 fs-8">
             <?php if ($this->params->get('show_page_heading')) : ?>
                 <div class="page-header">
                     <h1>
@@ -35,32 +35,31 @@ $back = (is_string($ref) && str_starts_with($ref, $root)) ? $ref : Route::_('ind
                 </div>
             <?php endif; ?>
 
+            <a href="<?php echo htmlspecialchars($back, ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-link fs-2 text-decoration-none p-0">
+                <i class="fa-regular fa-circle-xmark align-middle"></i>
+            </a>
+
             <?php if ($this->params->get('menu_image') != '') : ?>
-                <div class="bg-secondary p-2 text-center">
+                <div class="p-2 text-center">
                     <?php echo HTMLHelper::_('image', $this->params->get('menu_image'), 'logo alt' , ['class' => 'w-50']); ?>
                 </div>
             <?php endif; ?>
 
-            <form id="user-registration" action="<?php echo Route::_('index.php?option=com_users&task=remind.remind'); ?>" method="post" class="com-users-remind__form form-validate form-horizontal well px-5">
+            <form id="user-registration" action="<?php echo Route::_('index.php?option=com_users&task=remind.remind'); ?>" method="post" class="com-users-remind__form form-validate form-horizontal well">
                 <?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
                     <fieldset>
                         <?php if (isset($fieldset->label)) : ?>
-                            <legend class="text-justify py-3 fs-13 text-secondary"><?php echo Text::_($fieldset->label); ?></legend>
+                            <legend class="text-justify py-3 fs-7 text-secondary"><?php echo Text::_($fieldset->label); ?></legend>
                         <?php endif; ?>
                         <?php echo $this->form->renderFieldset($fieldset->name); ?>
                     </fieldset>
                 <?php endforeach; ?>
                 <div class="com-users-remind__submit control-group text-center">
                     <div class="controls d-grid gap-2 pb-2">
-                        <button type="submit" class="btn btn-danger fs-8 rounded-0 validate">
+                        <button type="submit" class="btn btn-danger fs-8 validate">
                             <?php echo Text::_('JSUBMIT'); ?>
                         </button>
                     </div>
-                    <hr class="border border-secondary border-1 opacity-25">
-                    <a href="<?php echo htmlspecialchars($back, ENT_QUOTES, 'UTF-8'); ?>" class="btn btn-link fs-8 rounded-0 text-decoration-none fs-9 p-0">
-                        <?php echo Text::_('TPL_HWDCITY_BACK'); ?>
-                        <i class="fa fa-arrow-left align-middle"></i>
-                    </a>
                 </div>
                 <?php echo HTMLHelper::_('form.token'); ?>
             </form>
