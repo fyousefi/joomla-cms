8f5e5838f94d4802ac8ee7406ac4b643acdef67f
fyousefi <f.yousefi2@gmail.com>
2025-11-02 17:34:29 +0330

    feat(template): center-crop full-width intro images via object-fit cover and anchor h-100

--- templates/hwdcity/html/layouts/joomla/content/intro_image.php
+++ templates/hwdcity/html/layouts/joomla/content/intro_image.php
@@ -71,10 +71,21 @@ $badgeLblFs  = $isFullLayout ? 'fs-6' : 'fs-13';
 
 // When full-width, drop the default bottom margin on the <figure>
 $figureMargin = $isFullLayout ? 'mb-0' : 'mb-3';
+
+// $layoutAttr
+if ($isFullLayout) {
+    // Make the image cover the fixed-height figure and stay centered
+    $layoutAttr['class'] = trim(($layoutAttr['class'] ?? '') . ' w-100 h-100 object-fit-cover');
+    // Bootstrap lacks object-position utility → inline style for center
+    $layoutAttr['style'] = trim(($layoutAttr['style'] ?? '') . ' object-position:center;');
+}
+
 ?>
 <figure class="<?php echo $this->escape($imgclass); ?> item-image position-relative <?php echo $figureMargin; ?> <?php echo $heightClass; ?>">
     <?php if ($params->get('link_intro_image') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
-        <a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>" title="<?php echo $this->escape($displayData->title); ?>">
+        <a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"
+           title="<?php echo $this->escape($displayData->title); ?>"
+            <?php echo $isFullLayout ? 'class="d-block h-100"' : ''; ?>>
             <?php echo !$isFullLayout ? '<span class="triangle-up position-absolute bottom-0 mx-5"></span>' : '' ?>
             <?php echo LayoutHelper::render('joomla.html.image', $layoutAttr); ?>
         </a>
